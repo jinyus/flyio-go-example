580d65685a573808c3d56810ee0df204e0d4d075
jinyus <starboymc4@gmail.com>
2023-02-27 14:09:53 -0500

    add /health

--- app.go
+++ app.go
@@ -20,11 +20,6 @@ func main() {
 
 	}
 
-	// viper.AutomaticEnv()
-	// fmt.Println("config file", viper.GetString("env"))
-	// fmt.Println("env", viper.Get("NAME"))
-	// fmt.Println("env os", os.Getenv("NAME"))
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := map[string]string{
 			"Region": os.Getenv("FLY_REGION"),
@@ -33,15 +28,11 @@ func main() {
 		t.ExecuteTemplate(w, "index.html.tmpl", data)
 	})
 
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	log.Println("listening on", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
-
-// func init() {
-// 	viper.SetConfigName("config") // name of config file (without extension)
-// 	viper.AddConfigPath(".")      // optionally look for config in the working directory
-// 	err := viper.ReadInConfig()   // Find and read the config file
-// 	if err != nil {               // Handle errors reading the config file
-// 		panic(fmt.Errorf("fatal error config file: %w", err))
-// 	}
-// }
